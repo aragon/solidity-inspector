39427ffd9cb727f58bdadec9ffdb3af9e8c490fb
vitiko <vitiko@mail.ru>
2017-01-13 15:39:46 +0300

    add eslint and reformatted

--- cli.js
+++ cli.js
@@ -2,16 +2,16 @@
 
 /* eslint no-console: 0 */
 
-var argv = require("yargs").argv;
-var SolidityStructure = require("./index.js");
+const argv = require('yargs').argv;
+const SolidityStructure = require('./index.js');
 
 
 var structure;
 
 
 if (argv.source) {
-    structure = SolidityStructure.parse(argv.source);
+  structure = SolidityStructure.parse(argv.source);
 } else {
-    structure = SolidityStructure.parseFile(argv._[0]);
+  structure = SolidityStructure.parseFile(argv._[0]);
 }
 console.log(JSON.stringify(structure.toJSON(), null, 2));
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
-const path = require("path");
-const ContractStructure = require("./lib/ContractStructure");
+const fs = require('fs');
+const path = require('path');
+const ContractStructure = require('./lib/ContractStructure');
 
 module.exports = {
-    parse: function (source, options = {}) {
-        return new ContractStructure(source, options);
-    },
+  parse: function (source, options = {}) {
+    return new ContractStructure(source, options);
+  },
 
-    parseFile: function (file, options = {}) {
+  parseFile: function (file, options = {}) {
 
-        if (!file) throw new Error ('SolidityStructure.parseFile : File path not provided');
-        options.filePath = path.resolve(file);
+    if (!file) throw new Error('SolidityStructure.parseFile : File path not provided');
+    options.filePath = path.resolve(file);
 
-        if (!fs.existsSync (options.filePath))
-            throw new Error ('SolidityStructure: file "'+options.filePath+'" not exists');
+    if (!fs.existsSync(options.filePath))
+      throw new Error('SolidityStructure: file "' + options.filePath + '" not exists');
 
-        return this.parse(fs.readFileSync(options.filePath, {encoding: "utf8"}), options);
-    }
+    return this.parse(fs.readFileSync(options.filePath, { encoding: 'utf8' }), options);
+  }
 };
