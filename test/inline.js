e1f1bb53939a1c5702523b1b90a77f9b6cb6fbb7
vitiko <vitiko@mail.ru>
2017-02-21 16:30:11 +0300

    returnsSeq in function

--- test/inline.js
+++ test/inline.js
@@ -29,7 +29,7 @@ contract test {
         parents: {},
         structs: {},
         events: {},
-        enums : {},
+        enums: {},
         functions: {
           multiply: {
             name: 'multiply',
@@ -38,7 +38,7 @@ contract test {
             paramsSeq: ['a'],
             params: {
               a: {
-                name : 'a',
+                name: 'a',
                 type: 'uint'
               }
             },
@@ -46,7 +46,11 @@ contract test {
               d: {
                 type: 'uint'
               }
-            }
+            },
+            returnsSeq: [
+              'd'
+            ]
+
           }
         },
         constantFunctions: {}
